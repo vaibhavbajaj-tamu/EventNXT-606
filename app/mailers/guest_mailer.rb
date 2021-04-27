c6e396f750226d0779bb89c1357ae3aa872de6f4
ruilian <evelynnngao@gmail.com>
2021-04-27 04:05:56 +0000

    add template for RSVP confirmation page, now RSVP form can send out emails

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -7,4 +7,10 @@ class GuestMailer < ApplicationMailer
         @url = 'localhost:3000/forms/submit?email='+email+'&maxtic='+maxtic
         mail(to: @guest.email_address, subject: 'Welcome to My Awesome Site')
     end
+    
+    def rsvp_confirmation_email(email, number)
+        @email = email
+        @number = number
+        mail(to: 'evelynnngao@gmail.com', subject: 'Congratulation on signing up the RSVP form')
+    end
 end
