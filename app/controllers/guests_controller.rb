b270bac7928a096d32c16257fe839a04da91d2b6
EC2 Default User <ec2-user@ip-172-31-4-31.us-east-2.compute.internal>
2021-10-31 23:05:18 +0000

    testing 2

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -5,7 +5,7 @@ class GuestsController < ApplicationController
   # end
   
   def send_email_invitation
-    puts "email send try"
+    flash[:notice] = "try to send email"
     event = Event.find(params[:event_id])
     guest = Guest.find(params[:id])
     if guest.booking_status == 'Yes' or guest.booking_status == 'No'
