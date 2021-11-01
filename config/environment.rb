2a93cdfe1dcdfcf48ffd095d2f89662acc1d3296
EC2 Default User <ec2-user@ip-172-31-4-31.us-east-2.compute.internal>
2021-11-01 22:50:00 +0000

    changed domain name to my app in environmentrb

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :port           => 587,
-  :address        => "smtp.gmail.com"
+  :address        => "smtp.gmail.com",
   :user_name      => 'tmpemailfortesting@gmail.com',
   :password       => 'simplepassword',
   :domain         => 'sheltered-forest-93990.herokuapp.com',
