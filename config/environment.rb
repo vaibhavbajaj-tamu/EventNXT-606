4b01bf5e8a120d4d0cf08950a8ffdde2eac94733
EC2 Default User <ec2-user@ip-172-31-4-31.us-east-2.compute.internal>
2021-11-01 23:44:10 +0000

    mailgun api test

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :port           => 587,
-  :address        => "smtp.gmail.com",
-  :user_name      => 'tmpemailfortesting@gmail.com',
-  :password       => 'simplepassword',
-  :domain         => 'sheltered-forest-93990.herokuapp.com',
-  :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   :port           => 587,
+#   :address        => "smtp.gmail.com",
+#   :user_name      => 'tmpemailfortesting@gmail.com',
+#   :password       => 'simplepassword',
+#   :domain         => 'sheltered-forest-93990.herokuapp.com',
+#   :authentication => :plain,
+# }
+# ActionMailer::Base.delivery_method = :smtp
