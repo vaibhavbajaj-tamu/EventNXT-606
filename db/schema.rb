3b3ad0922c85e8bf2e13d94f2ff3a87e95798473
Sia9498 <asees9498@gmail.com>
2021-11-08 09:05:38 +0000

    Seat Category table in the Show page

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,32 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210426211246) do
+ActiveRecord::Schema.define(version: 2021_11_08_084113) do
 
   create_table "events", force: :cascade do |t|
-    t.string  "title"
-    t.string  "date"
+    t.string "title"
+    t.string "date"
     t.integer "total_seats"
-    t.string  "box_office_customers"
+    t.string "box_office_customers"
     t.integer "total_seats_box_office"
     t.integer "total_seats_guest"
     t.integer "balance"
+    t.string "seat_category"
   end
 
   create_table "guests", force: :cascade do |t|
-    t.string  "email_address"
+    t.string "email_address"
     t.integer "event_id"
-    t.string  "first_name"
-    t.string  "last_name"
-    t.string  "affiliation"
-    t.string  "added_by"
-    t.string  "guest_type"
-    t.string  "seat_category"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "affiliation"
+    t.string "added_by"
+    t.string "guest_type"
+    t.string "seat_category"
     t.integer "max_seats_num"
-    t.string  "booking_status"
+    t.string "booking_status"
     t.integer "total_booked_num"
+    t.index ["event_id"], name: "index_guests_on_event_id"
   end
 
-  add_index "guests", ["event_id"], name: "index_guests_on_event_id"
+  create_table "seat_category_details", force: :cascade do |t|
+    t.string "event_title"
+    t.string "seat_category"
+    t.integer "total_seats"
+    t.integer "vip_seats"
+    t.integer "non_vip_seats"
+    t.integer "balance"
+  end
 
 end
