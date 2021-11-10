acc3e055e444e495ecfd97e1af7ef501fb8775d5
Pavankumar P K <pavankakkanavari920@gmail.com>
2021-11-10 09:12:01 -0600

    Removing the api-key

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  password: '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', # This is the secret sendgrid API key which was issued during API key creation
+  password: '<api-key>', # This is the secret sendgrid API key which was issued during API key creation
   domain: 'form4s.herokuapp.com',
   address: 'smtp.sendgrid.net',
   port: 587,
