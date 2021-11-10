4615582aa12929931c3d56f52fca3215b4b6b616
EC2 Default User <ec2-user@ip-172-31-4-31.us-east-2.compute.internal>
2021-11-10 03:18:16 +0000

    try again again

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,6 +21,7 @@ class EventsController < ApplicationController
   end
   
   def open_existed_spreadsheet
+    @event_pic = params[:event_picture] #new
     event = Event.find_by(title: params[:event_title])
     if !event
       flash[:notice] = "Cannot find the event #{params[:event_title]}."
@@ -32,6 +33,7 @@ class EventsController < ApplicationController
   
   def show
     @event = Event.find(params[:id])
+    @event_pic = params[:event_picture] #new
     @guests = @event.guests
     @guest_params = Guest.column_names
     fixed_params = ['id', 'event_id', 'booking_status', 'total_booked_num']
