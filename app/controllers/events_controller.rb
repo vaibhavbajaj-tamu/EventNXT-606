162c76779e68454bcaf7fb3498e265021981ad22
EC2 Default User <ec2-user@ip-172-31-7-240.ec2.internal>
2021-11-15 19:28:03 +0000

    putting the missing lines in eventcontroller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,4 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!
   def index
     # @events = Hash.new()
     # for event in Event.all
@@ -10,9 +9,6 @@ class EventsController < ApplicationController
       # puts(event.inspect)
       # puts(Event.column_names.inspect)
     # end
-  end
-  
-  def create_event
   end
   
   def import_new_spreadsheet
@@ -62,6 +58,28 @@ class EventsController < ApplicationController
   end
  #^Seat Categories Code //////////////////
   
+  def seat_categories
+    redirect_to root_path and return
+  end
+  
+  class SeatWiseRows
+    attr_accessor :vip_seats
+    attr_accessor :non_vip_seats
+    attr_accessor :seat_category
+    attr_accessor :total_seats
+    attr_accessor :balance
+    #attr_accessor :model_name
+    
+    def initialize(a, b, c, d, e)
+      #@model_name = 'SeatWiseRows'
+      @total_seats = a
+      @vip_seats = b
+      @non_vip_seats = c
+      @seat_category = d
+      @balance = e
+    end
+  end
+  
   def show
     @event = Event.find(params[:id])
     #@event_pic = "https://www.lavendascloset.com/wp-content/uploads/2016/10/FashionNXT-103.jpg"
@@ -81,8 +99,45 @@ class EventsController < ApplicationController
     fixed_params.each do |fixed_param|
       @guest_params.delete(fixed_param)
     end
-    # puts(@guest_params)
+    
     @customers = @event.box_office_customers.split('#row#').map{|row| row.split('#cell#')}
+    #puts(@customers[1])
+    @customers[1..-1].each do |row|
+      @seat_category_set.add?(row[9])
+      
+      if @bo_seat_wise_split.keys.include?(row[9]) == true
+        @bo_seat_wise_split[row[9]] += row[24].to_i
+      else
+        @bo_seat_wise_split[row[9]] = row[24].to_i
+      end
+    end
+    
+    @guests.each do |guest|
+      puts(guest)
+      @seat_category_set.add?(guest[:seat_category])
+      
+      if @vip_seat_wise_split.keys.include?(guest[:seat_category]) == true
+        @vip_seat_wise_split[guest[:seat_category]] += guest[:max_seats_num]    #change to guest[:total_booked_num]
+      else
+        @vip_seat_wise_split[guest[:seat_category]] = guest[:max_seats_num]     #change to guest[:total_booked_num]
+      end
+    end
+    
+    @seat_category_set.each do |category_name|
+      vip_seats = 0
+      non_vip_seats = 0
+      if @vip_seat_wise_split.keys.include?(category_name)
+        vip_seats = @vip_seat_wise_split[category_name]
+      end
+      if @bo_seat_wise_split.keys.include?(category_name)
+        non_vip_seats = @bo_seat_wise_split[category_name]
+      end
+     
+      total_seats = 500
+      balance = total_seats - vip_seats - non_vip_seats
+      newEntry = SeatWiseRows.new(total_seats, vip_seats, non_vip_seats, category_name, balance)
+      @total_seat_wise_split.append(newEntry)
+    end
     
     #new
     @customers[1..-1].each do |row|
@@ -128,6 +183,7 @@ class EventsController < ApplicationController
       @event.total_seats_guest += guest.total_booked_num
     end
     @event.balance = @event.total_seats - @event.total_seats_box_office - @event.total_seats_guest
+    
   end
   
   # def new
@@ -156,7 +212,7 @@ class EventsController < ApplicationController
       redirect_to @event
     else
       flash[:notice] = "Failed to update the event information."
-      redirect_to @event
+      redirect_to @event 
     end
   end
   
