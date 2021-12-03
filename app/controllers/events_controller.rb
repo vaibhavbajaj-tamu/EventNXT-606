23b3a85a70e816e316ee7cb21a547d2cc6239587
leon990x <leon990x@gmail.com>
2021-12-03 11:10:22 -0600

    fixed excel import

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -82,73 +82,74 @@ class EventsController < ApplicationController
   end
  #^Seat Categories Code //////////////////
   
-  def show
-    @event = Event.find(params[:id])
-    $event_pic = $event_pic.to_s #new
-    
-    $levels = params[:event_level]
-    @guests = @event.guests
-    @guest_params = Guest.column_names
+ def show
+  @event = Event.find(params[:id])
+  $event_pic = $event_pic.to_s #new
+  
+  @guests = @event.guests
+  @guest_params = Guest.column_names
 
-    #seat category additions
-    @bo_seat_wise_split = {}
-    @vip_seat_wise_split = {}
-    @seat_category_set = Set.new
-    @total_seat_wise_split = []
-    #end seat category additions
+  #seat category additions
+  @bo_seat_wise_split = {}
+  @vip_seat_wise_split = {}
+  @seat_category_set = Set.new
+  @total_seat_wise_split = []
+  #end seat category additions
 
-    fixed_params = ['id', 'event_id', 'booking_status', 'total_booked_num']
-    fixed_params.each do |fixed_param|
-      @guest_params.delete(fixed_param)
-    end
-    # puts(@guest_params)
-    @customers = @event.box_office_customers.split('#row#').map{|row| row.split('#cell#')}
-    
-    #new
-    @customers[1..-1].each do |row|
-      @seat_category_set.add?(row[9])
-      
-      if @bo_seat_wise_split.keys.include?(row[9]) == true
-        @bo_seat_wise_split[row[9]] += row[24].to_i
-      else
-        @bo_seat_wise_split[row[9]] = row[24].to_i
-      end
-    end
-
-    @guests.each do |guest|
-      puts(guest)
-      @seat_category_set.add?(guest[:seat_category])
-      
-      if @vip_seat_wise_split.keys.include?(guest[:seat_category]) == true
-        @vip_seat_wise_split[guest[:seat_category]] += guest[:max_seats_num]    #change to guest[:total_booked_num]
-      else
-        @vip_seat_wise_split[guest[:seat_category]] = guest[:max_seats_num]     #change to guest[:total_booked_num]
-      end
-    end
-    
-    @seat_category_set.each do |category_name|
-      vip_seats = 0
-      non_vip_seats = 0
-      if @vip_seat_wise_split.keys.include?(category_name)
-        vip_seats = @vip_seat_wise_split[category_name]
-      end
-      if @bo_seat_wise_split.keys.include?(category_name)
-        non_vip_seats = @bo_seat_wise_split[category_name]
-      end
-     
-      total_seats = 500
-      balance = total_seats - vip_seats - non_vip_seats
-      newEntry = SeatWiseRows.new(total_seats, vip_seats, non_vip_seats, category_name, balance)
-      @total_seat_wise_split.append(newEntry)
-    end
-    #new end
-
-    @event.total_seats_guest = 0
-    for guest in @guests
-      @event.total_seats_guest += guest.total_booked_num
-    end
-    @event.balance = @event.total_seats - @event.total_seats_box_office - @event.total_seats_guest
+  fixed_params = ['id', 'booking_status', 'total_booked_num']
+  fixed_params.each do |fixed_param|
+    @guest_params.delete(fixed_param)
   end
+  @customers = @event.box_office_customers.split('#row#').map{|row| row.split('#cell#')}
+  
+  #new
+  @customers[1..-1].each do |row|
+    @seat_category_set.add?(row[9])
+    
+    if @bo_seat_wise_split.keys.include?(row[9]) == true
+      @bo_seat_wise_split[row[9]] += row[24].to_i
+    else
+      @bo_seat_wise_split[row[9]] = row[24].to_i
+    end
+  end
+  
+  @guests.each do |guest|
+    puts(guest)
+    @seat_category_set.add?(guest[:seat_category])
+    
+    if @vip_seat_wise_split.keys.include?(guest[:seat_category]) == true
+      @vip_seat_wise_split[guest[:seat_category]] += guest[:max_seats_num]    #change to guest[:total_booked_num]
+    else
+      @vip_seat_wise_split[guest[:seat_category]] = guest[:max_seats_num]     #change to guest[:total_booked_num]
+    end
+  end
+  
+  @seat_category_set.each do |category_name|
+    vip_seats = 0
+    non_vip_seats = 0
+    if @vip_seat_wise_split.keys.include?(category_name)
+      vip_seats = @vip_seat_wise_split[category_name]
+    end
+    if @bo_seat_wise_split.keys.include?(category_name)
+      non_vip_seats = @bo_seat_wise_split[category_name]
+    end
+   
+    total_seats = 500
+    balance = total_seats - vip_seats.to_i - non_vip_seats.to_i
+    newEntry = SeatWiseRows.new(total_seats, vip_seats, non_vip_seats, category_name, balance)
+    @total_seat_wise_split.append(newEntry)
+  end
+  #new end
+
+  @event.total_seats_guest = 0
+  for guest in @guests
+    
+    if guest.total_booked_num!=nil
+        @event.total_seats_guest += guest.total_booked_num
+    end
+  end
+  @event.balance = @event.total_seats - @event.total_seats_box_office - @event.total_seats_guest
+end
   
   # def new
   #   @event = Event.new
