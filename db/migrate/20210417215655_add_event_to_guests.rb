0af3c9965ca50857ce60cf7c580447d838a0c486
leon990x <leon990x@gmail.com>
2021-12-05 14:46:25 -0600

    deleted duplicate migrations from local

--- db/migrate/20210417215655_add_event_to_guests.rb
+++
@@ -1,5 +0,0 @@
-class AddEventToGuests < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :guests, :event, index: true, foreign_key: true
-  end
-end
--- db/migrate/20210426211246_remove_type_from_guests.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTypeFromGuests < ActiveRecord::Migration[5.2]
-  def change
-    #remove_column :guests, :type, :string
-  end
-end
