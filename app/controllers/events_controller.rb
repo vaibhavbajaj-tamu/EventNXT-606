db64533b18e0504d40363a73c9f5f3d87f610cf2
EC2 Default User <ec2-user@ip-172-31-3-200.ec2.internal>
2021-12-05 00:05:36 +0000

    working_changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -63,20 +63,22 @@ class EventsController < ApplicationController
  #^Seat Categories Code //////////////////
   
   def show
-    @event = Event.find(params[:id])
+    @event = Event.find(1)
     #@event_pic = "https://www.lavendascloset.com/wp-content/uploads/2016/10/FashionNXT-103.jpg"
     $event_pic = $event_pic.to_s #new
-    
+    @seat_types = SeatingType.all
     @guests = @event.guests
     @guest_params = Guest.column_names
-
+    @cat = []
     #seat category additions
     @bo_seat_wise_split = {}
     @vip_seat_wise_split = {}
     @seat_category_set = Set.new
     @total_seat_wise_split = []
+    @seat_type_split = []
     #end seat category additions
-
+    
+    
     fixed_params = ['id', 'booking_status', 'total_booked_num']
     fixed_params.each do |fixed_param|
       @guest_params.delete(fixed_param)
@@ -117,6 +119,10 @@ class EventsController < ApplicationController
      
       total_seats = 500
       balance = total_seats - vip_seats - non_vip_seats
+      hash = { total_seats: total_seats, vips_seats: vip_seats, non_vip_seats: non_vip_seats, category_name: category_name,balance: balance }
+      
+# {:bacon=>:protein, :apple=>:fruit}
+      @cat.append(hash)
       newEntry = SeatWiseRows.new(total_seats, vip_seats, non_vip_seats, category_name, balance)
       @total_seat_wise_split.append(newEntry)
     end
@@ -130,6 +136,38 @@ class EventsController < ApplicationController
       end
     end
     @event.balance = @event.total_seats - @event.total_seats_box_office - @event.total_seats_guest
+    string_error = ""
+    if params[:reconcile].nil?
+      @seat_types.each do |seat_type|
+        @seat_type_split.append(seat_type)
+      end
+    else
+      @seat_types.each do |seat_type|
+        @seat_type_split.append(seat_type)
+      end
+      for @category in @cat
+        found = 0
+        for seat_type in @seat_type_split
+          if seat_type[:seat_category] == @category[:category_name]
+            found = 1
+            if @category[:vip_seats].nil?
+              @category[:vip_seats] = 0
+            end
+            balance_seat = seat_type[:total_seat_count] - @category[:vip_seats] - @category[:non_vip_seats]
+            seat_type[:balance_seats] = balance_seat
+            seat_type[:box_office_seat_count] = @category[:non_vip_seats]
+          end
+        end
+        if found == 0
+          string_error  += @category[:category_name] + " ,"
+        end
+      end
+    end
+    if string_error!= ""
+      string_error += "is not in the database."
+      puts(string_error)
+      flash[:notice] = string_error
+    end
   end
   
   # def new
@@ -150,7 +188,16 @@ class EventsController < ApplicationController
   # def edit
   #   @event = Event.find(params[:id])
   # end
-
+  def reconcile
+    @seat_type_split = []
+    @seat_types = SeatingType.all
+    @seat_types.each do |seat_type|
+      #newEntry = SeatWiseNonExcelRows.new(total_seats, vip_seats, non_vip_seats, category_name, balance)
+      @seat_type_split.append(seat_type)
+    end
+    #render event_path(1)
+  end
+  
   def update
     @event = Event.find(params[:id])
 
