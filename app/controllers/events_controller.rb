73c55c00b5756a58f43dab399a3ada2bf9b37ad6
Joseph Muir <jgmuir2021@tamu.edu>
2022-04-10 23:41:26 -0500

    edit functionality works

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,7 +14,7 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-    @event.user_id = current_user
+
     #render json: {event: event}
     if @event.save
       redirect_to @event
@@ -22,7 +22,18 @@ class EventsController < ApplicationController
     #render_valid(event)
   end
 
+  def edit
+    @event = Event.find(params[:id])
+  end
+
   def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit
+    end
   end
 
   def destroy
