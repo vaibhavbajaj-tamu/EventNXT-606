cedac0b30d5bfa82e8a8241b6c48ced5af37e202
Aash19 <53142153+Aash19@users.noreply.github.com>
2022-12-08 11:05:13 -0600

    updating to add purchase tickets template

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -12,6 +12,12 @@ class GuestMailer < ApplicationMailer
         mail(to: @guest.email, subject: "#{@event.title} - Invitation")
    end
     
+   def purchase_tickets_email(referral_email, event, guest)
+        @event=event
+        @guest = guest
+        mail(to: referral_email, subject: "Purchase Tickets")
+   end
+    
    def rsvp_confirmation_email(event, guest)
         @event = event
         @guest = guest
