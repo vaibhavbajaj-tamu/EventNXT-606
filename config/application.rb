ed653cdd26e436ab5372bdd9d733414f38ca17f6
vaibhavbajaj <vaibhavbajaj15@gmail.com>
2023-02-24 00:21:29 -0600

    commented gitignore

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'dotenv/load'
-require 'bin/rails/all'
+require 'rails/all'
 require 'set'
 
 # Require the gems listed in Gemfile, including any gems
